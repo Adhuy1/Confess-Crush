b2e6b41bd77fdfa09585f5ce72110d766a743f76
Adhuy1 <89148500+Adhuy1@users.noreply.github.com>
2022-10-09 21:48:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'babe à',
-    introDesc: `nếu như mỹ nhân kế
-    có tác dụng ... mình ko ngại
-    mỗi ngày đều cười với cậu`,
+    titleWeb: "Tặng Lão Bà Tử Thiên'z",
+    introTitle: 'Hello Word ( Xin Chào Cả Thế Giới )',
+    introDesc: `Vô Tình Nhặt Đc Lão Bà ;))`,
     btnIntro: '^^HiHi^^',
-    title: 'phải chăng cậu đã yêu từ ngay cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng là như vậy        ',
-    btnYes: 'vẫn cứ là thích cậu<33',
-    btnNo: 'ko, cậu à :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao cậu lại nói yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'yêu thì yêu mà ko yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. yêu cậu nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón cậu nhé.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/profile.php?id=100064758293789'
+    title: 'Phải chăng em đã yêu anh ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'nếu em ko trl mà thoát ra có nghĩa là em thích anh r ',
+    btnYes: 'Thích Anh  <33',
+    btnNo: 'Anh Mơ À :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Em Yêu Anh Từ Cái Nhìn Đầu Tiên Rồi Ạ',
+    mess: 'Mình biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Ad Huy Tặng Lão Bà 1 Bất Ngờ ;)).',
+    btnAccept: 'Chấp Nhận <3',
+    messLink: 'https://www.facebook.com/huytrumsvzxc' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
