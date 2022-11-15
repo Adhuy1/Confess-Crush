0144fac42afcc0b051f4309a64a0e1bba5bd5679
Adhuy1 <89148500+Adhuy1@users.noreply.github.com>
2022-11-16 02:27:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
     titleWeb: "Ad Huy",
     introTitle: 'Hello',
-    introDesc: `trả lời hết để nhận mã thẻ`,
+    introDesc: `hỏi tí nhé `,
     btnIntro: '^^HiHi^^',
     title: 'ta có phải là người đẹp trai nhất thiên hạ ko 🥰',
     desc: 'nếu em ko trl mà thoát ra có nghĩa là em thích anh r ',
     btnYes: 'đr, anh đẹp trai nhất  <33',
     btnNo: 'Anh Mơ À :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại nói anh dệp trai nhất <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại nói anh đẹp trai nhất <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Mã 795650159530 , vì anh đep trai 😂',
+    reply: 'vì anh đep trai 😂',
     mess: 'anh biết mà 😘😘',
     messDesc: ' ;)).',
     btnAccept: 'Chấp Nhận <3',
-    messLink: 'https://www.facebook.com/huytrumsvzxc' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: ' https://m.me/huytrumsvzxc '   link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
